0930e2c43214bd5f2563cdbf95188af75a644e4b
Manuel Landin Gomez <manuel.landin@outlook.es>
2023-05-15 22:54:24 +0200

    fix some errors

--- WebScraper.py
+++ WebScraper.py
@@ -1,17 +1,16 @@
 import json
 from bs4 import BeautifulSoup
 import requests
-from scripts.read_config import read_conf
 
 class WebScraper:
-    DEFAULT_CONFIG_URI = "./config/options.conf", mode="r", encoding="UTF8"
-    
     def __init__(self, url: str) -> None:
+        self.DEFAULT_CONFIG_URI = "config/options.json"
         self._url = url
         self._all_data = {}
-        self._soup = self._get_soup()
-        self._title = self._scrap_title()
-        self._description = self._scrap_description()
+        self._get_soup()
+        self._get_conf()
+        self._scrap_title()
+        self._scrap_description()
         
     def _get_soup(self) -> None:
         try:
@@ -19,17 +18,18 @@ class WebScraper:
             if response.status_code == 200:
                 html = response.content
                 self._soup = BeautifulSoup(html, 'lxml')
-            raise requests.RequestException('status code: ' + response.status_code)
+            else:
+                raise requests.RequestException('status code: ' + response.status_code)
         except:
             raise requests.RequestException('Invalid web site')
             
     def _get_conf(self) -> dict:
-    try:
-        file = open(DEFAULT_CONFIG_URI)
-        config = json.loads(file.read())
-        file.close()
-    except:
-        return None
+        try:
+            file = open(file=self.DEFAULT_CONFIG_URI, mode="r", encoding="UTF8")
+            self._conf = json.loads(file.read())
+            file.close()
+        except:
+            return None
 
     def _scrap_title(self) -> None:
         try:
@@ -40,7 +40,7 @@ class WebScraper:
     def _scrap_description(self) -> None:
         try:
             ctrl_size = 0
-            for label in self._get_conf().get("labels"):
+            for label in self._conf.get("labels"):
                 for attr in label.get("attrs"):
                     description = self._soup.find(label.get("label"), attrs=attr)
                     if description and len(description['content']) > ctrl_size:
--- main.py
+++ main.py
@@ -4,4 +4,5 @@ url = "https://arxiv.org/abs/2304.04947"
 
 ws = WebScraper(url)
 
-print(ws.get_all_data)
+print(ws.get_title)
+print(ws.get_description)
